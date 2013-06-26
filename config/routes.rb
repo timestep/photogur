f8fb68d4bbee38ff5ab3005f285fd5b9a6ea5622
timestep <jbosshellokitty@gmail.com>
2013-06-26 13:18:30 -0400

    refactoring CRUD w/ resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Photogur::Application.routes.draw do
 
+  resources :pictures
   root :to => "pictures#index"
-
-  post 'pictures' => "pictures#create" # this is a new line of code
-
-  get 'pictures' => "pictures#index"
-
-  get 'pictures/new' => 'pictures#new' # this is also a new line of code
-  get 'pictures/:id' => 'pictures#show'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
