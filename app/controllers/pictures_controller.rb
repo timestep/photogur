1574d5578f9a7b61a3ba28b0e12ac416602ec583
timestep <jbosshellokitty@gmail.com>
2013-06-26 13:00:43 -0400

    database migration

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,33 +1,18 @@
 class PicturesController < ApplicationController
 
-	def show
-		@pictures = [
-			{
-				:title => "Old McDonalds",
-				:artist => 'The Weak Man',
-				:url => 'http://i.imgur.com/yn2aG2K.jpg'
-			},
-			{
-				:title => 'Chickens',
-				:artist => 'Ronald',
-				:url => 'http://i.imgur.com/60vVp81.jpg'
-			},
-			{
-				:title => "Burgers and Fries",
-				:artist => "Tarintino",
-				:url => 'http://i.imgur.com/HiX0dTL.jpg'
-			}
-		]	
-		@picture = @pictures[params[:id].to_i]
+  def index
+    @pictures = Picture.all
+  end
 
-	end
-	
-	def new
-	end
+  def show
+    @picture = Picture.find(params[:id])
+  end
 
-	def create
-		render :text => 'Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}'
-	end
+  def new
+  end
 
+  def create
+    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Picture.create!(
+	:title => "Old McDonalds",
+	:artist => 'The Weak Man',
+	:url => 'http://i.imgur.com/yn2aG2K.jpg'
+	)
+
+Picture.create!(
+	:title => 'Chickens',
+	:artist => 'Ronald',
+	:url => 'http://i.imgur.com/60vVp81.jpg'
+	)
+
+Picture.create!(
+	:title => "Burgers and Fries",
+	:artist => "Tarintino",
+	:url => 'http://i.imgur.com/HiX0dTL.jpg'
+)
