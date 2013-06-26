ca009d6999a453fa4c1f746637e03f50aa3c28c0
timestep <jbosshellokitty@gmail.com>
2013-06-26 11:53:03 -0400

    pictures work

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,24 @@
 class PicturesController < ApplicationController
 
 	def index
+		@pictures = [
+			{
+				:title => "Old McDonalds",
+				:artist => 'The Weak Man',
+				:url => 'http://i.imgur.com/yn2aG2K.jpg'
+			},
+			{
+				:title => 'Chickens',
+				:artist => 'Ronald',
+				:url => 'http://i.imgur.com/60vVp81.jpg'
+			},
+			{
+				:title => "Burgers and Fries",
+				:artist => "Tarintino",
+				:url => 'http://i.imgur.com/HiX0dTL.jpg'
+			}
+		]	
 	end
-	
+
 
 end
