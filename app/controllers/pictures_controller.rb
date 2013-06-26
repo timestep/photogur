c1d4ddf25e27758817c9a2643cd0dd5d0cf086d2
timestep <jbosshellokitty@gmail.com>
2013-06-26 11:58:40 -0400

    making each pic at own url

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -20,5 +20,28 @@ class PicturesController < ApplicationController
 		]	
 	end
 
+	def picture0
+		@picture = { 
+			:title => "Old McDonalds",
+			:artist => 'The Weak Man',
+			:url => 'http://i.imgur.com/yn2aG2K.jpg'
+		}
+	end
+
+	def picture1
+		@picture = {
+			:title => 'Chickens',
+			:artist => 'Ronald',
+			:url => 'http://i.imgur.com/60vVp81.jpg'
+		}
+	end
+
+	def picture2 
+		@picture = {
+			:title => "Burgers and Fries",
+			:artist => "Tarintino",
+			:url => 'http://i.imgur.com/HiX0dTL.jpg'
+		}
+	end
 
 end
