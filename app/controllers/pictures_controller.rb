e53bd6eaa7847447723db40473a34e5148a324e6
timestep <jbosshellokitty@gmail.com>
2013-06-26 12:09:34 -0400

    refactoring

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -20,28 +20,6 @@ class PicturesController < ApplicationController
 		]	
 	end
 
-	def picture0
-		@picture = { 
-			:title => "Old McDonalds",
-			:artist => 'The Weak Man',
-			:url => 'http://i.imgur.com/yn2aG2K.jpg'
-		}
-	end
-
-	def picture1
-		@picture = {
-			:title => 'Chickens',
-			:artist => 'Ronald',
-			:url => 'http://i.imgur.com/60vVp81.jpg'
-		}
-	end
-
-	def picture2 
-		@picture = {
-			:title => "Burgers and Fries",
-			:artist => "Tarintino",
-			:url => 'http://i.imgur.com/HiX0dTL.jpg'
-		}
-	end
+	@pictures = @pictures[params[:id].to_i]
 
 end
